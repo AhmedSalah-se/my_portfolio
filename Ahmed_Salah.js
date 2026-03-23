aa1155f9e32f913d77d14117859d77ec3f89a75f
A7MED77777 <ahmedsalahtawheed9@gmail.com>
2026-03-23 05:16:14 +0200

    Add files via upload

---
+++ Ahmed_Salah.js
@@ -0,0 +1,12 @@
+const elements = document.querySelectorAll('.fade-in');
+
+window.addEventListener('scroll', () => {
+  elements.forEach(el => {
+    const position = el.getBoundingClientRect().top;
+    const screenPosition = window.innerHeight / 1.2;
+
+    if(position < screenPosition) {
+      el.classList.add('show');
+    }
+  });
+});
